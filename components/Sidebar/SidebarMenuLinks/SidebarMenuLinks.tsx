210189a71d5258c21964387aef5bbc95a30ffa38
Sage <tuzzy08@gmail.com>
2021-03-25 15:16:19 +0100

    setup linting

--- components/Sidebar/SidebarMenuLinks/SidebarMenuLinks.tsx
+++ components/Sidebar/SidebarMenuLinks/SidebarMenuLinks.tsx
@@ -1,43 +1,47 @@
-import { Link, Box, Flex, Center, useColorModeValue, Text, HStack } from '@chakra-ui/react';
 import NextLink from 'next/link'
-import { Icon } from "@chakra-ui/react"
+import { Link, Icon } from '@chakra-ui/react'
 
+interface Props {
+  title: string
+  href: string
+  icon: any
+}
 
-export default function SubMenuLink({title, href, icon}) {
+export default function SubMenuLink({ title, href, icon }: Props) {
   return (
     // <Flex
-    // _hover={{       
+    // _hover={{
     //   color: '#e6cb2d',
     //   textDecoration: 'none',
     //   bg: 'black',
     //   cursor: 'pointer'
     // }}
     // >
-      <NextLink href={href} passHref>     
-        <Link
-          px={4}
-          py={3}
-          rounded={'sm'}
-          textAlign='justify'
-          fontFamily='body'
-          fontSize='md'
-          fontWeight='semibold'
-          textTransform='capitalize'
-          color='black'
-          _hover={{       
-            color: '#e6cb2d',
-            textDecoration: 'none',
-            bg: 'black',
-          }}
-          _focus={{
-            outline: 'none'
-          }}
-          >
-            <Icon as={icon} mx='5px' my='4px' />
-            {/* <Flex w='15px' h='27px'ml='7px'></Flex> */}
-          {title}
-        </Link>
-      </NextLink> 
+    <NextLink href={href} passHref>
+      <Link
+        px={4}
+        py={3}
+        rounded={"sm"}
+        textAlign="justify"
+        fontFamily="body"
+        fontSize="md"
+        fontWeight="semibold"
+        textTransform="capitalize"
+        color="black"
+        _hover={{
+          color: "#e6cb2d",
+          textDecoration: "none",
+          bg: "black",
+        }}
+        _focus={{
+          outline: "none",
+        }}
+      >
+        <Icon as={icon} mx="5px" my="4px" />
+        {/* <Flex w='15px' h='27px'ml='7px'></Flex> */}
+        {title}
+      </Link>
+    </NextLink>
     // </Flex>
-  )
+  );
 }
--- utils/api/initMiddleware.tsx
+++ utils/api/initMiddleware.tsx
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 export default function initMiddleware(middleware: Function) {
