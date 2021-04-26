9ddac0aaecbe843a6a9c2f01e4559398d3610b19
Sage <tuzzy08@gmail.com>
2021-04-26 23:53:22 +0100

    Minor refactoring

---
+++ utils/constants/areas.tsx
@@ -0,0 +1,11 @@
+const areas = [
+  { value: 'dLine', label: 'DLine' },
+  { value: 'trans-amadi', label: 'Trans-Amadi' },
+  { value: 'ogbum-nu-abali', label: 'Ogbum-nu-abali' },
+  { value: 'peter-odili', label: 'Peter Odili' },
+  { value: 'new-g-r-a', label: 'New G.R.A' },
+  { value: 'old-g-r-a', label: 'Old G.R.A' },
+  { value: 'stadium-road', label: 'Stadium Road' },
+]
+
+export default areas
